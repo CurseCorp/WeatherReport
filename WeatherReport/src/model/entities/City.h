e886d4d494104051fec145725e9e945f7a33db39
Manemitio811 <olegklish363@gmail.com>
2026-05-26 01:08:25 +0300

    Добавлена интеграция WeatherAPI для получения погодных данных

--- WeatherReport/src/model/entities/City.h
+++ WeatherReport/src/model/entities/City.h
@@ -1,6 +1,7 @@
 #ifndef CITY_H
 #define CITY_H
 
+#include <QObject>
 #include <QString>
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
@@ -11,4 +12,41 @@ class City : public QObject
 {
     Q_OBJECT
 
+public:
+    City(QObject *parent = nullptr);
+    ~City();
+
+    // Основные методы
+    void fetchCityFromAPI(const QString &cityName);
+    void saveToDatabase();
+
+    // Геттеры
+    int getId() const { return m_id; }
+    QString getName() const { return m_name; }
+    QString getDisplayName() const { return m_displayName.isEmpty() ? m_name : m_displayName; }
+    QString getCountry() const { return m_country; }
+    double getLatitude() const { return m_latitude; }
+    double getLongitude() const { return m_longitude; }
+
+signals:
+    void errorOccurred(const QString& error);
+    void dataSaved();
+    void cityFound(const QString& displayName);  // Сигнал когда город найден
+
+private slots:
+    void onApiReplyFinished(QNetworkReply *reply);
+
+private:
+    int m_id;
+    QString m_name;
+    QString m_displayName;
+    QString m_country;
+    double m_latitude;
+    double m_longitude;
+
+    QNetworkAccessManager *m_networkManager;
+    QSqlDatabase m_database;
+    bool connectToDatabase();
 };
+
+#endif // CITY_H
