4a8e2375579a8b11eb646c5226fa72c75ab56fa3
Infcurse <122828048+M1kr03x@users.noreply.github.com>
2026-05-25 17:01:10 +0300

    Update main.cpp

--- WeatherReport/src/main.cpp
+++ WeatherReport/src/main.cpp
@@ -7,11 +7,7 @@ int main(int argc, char *argv[]) {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-
-    // Создаем экземпляр твоей ViewModel
     WeatherViewModel viewModel;
-
-    // Регистрируем её в QML под именем "weatherViewModel"
     engine.rootContext()->setContextProperty("weatherViewModel", &viewModel);
 
     const QUrl url(u"qrc:/WeatherReport/view/Main.qml"_qs);
