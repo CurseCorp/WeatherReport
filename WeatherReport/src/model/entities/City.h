165f23b244b90e63cb5d0d8199d5c5e7c693511e
Manemitio811 <olegklish363@gmail.com>
2026-05-25 18:50:11 +0300

    Добавил заглушки для viewmodel

--- WeatherReport/src/model/entities/City.h
+++ WeatherReport/src/model/entities/City.h
@@ -1,1 +1,14 @@
-//213
+#ifndef CITY_H
+#define CITY_H
+
+#include <QString>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+class City : public QObject
+{
+    Q_OBJECT
+
+};
