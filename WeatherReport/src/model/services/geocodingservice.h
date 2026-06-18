25cd8538a33cf7e946a50a68efc81a25ff917255
Manemitio811 <olegklish363@gmail.com>
2026-06-19 00:13:01 +0300

    добавлен GeocodingService

---
+++ WeatherReport/src/model/services/geocodingservice.h
@@ -0,0 +1,40 @@
+#ifndef GEOCODINGSERVICE_H
+#define GEOCODINGSERVICE_H
+
+#include <QObject>
+#include <QVector>
+#include <QCache>
+#include <QTimer>
+#include <QNetworkAccessManager>
+#include "src/model/entities/City.h"
+
+class GeocodingService : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit GeocodingService(QObject *parent = nullptr);
+    ~GeocodingService();
+
+    void searchCities(const QString &query);
+
+signals:
+    void searchFinished(const QVector<CityData> &results);
+    void errorOccurred(const QString &error);
+
+private slots:
+    void performSearch();
+    void onReplyFinished(QNetworkReply *reply);
+
+private:
+    QNetworkAccessManager *m_networkManager;
+    QCache<QString, QVector<CityData>> m_cache;
+    QTimer m_debounceTimer;
+    QString m_pendingQuery;
+    QString m_apiKey;
+
+    QVector<CityData> parseSearchResults(const QByteArray &rawData);
+    void doSearch(const QString &query);
+};
+
+#endif // GEOCODINGSERVICE_H
--- WeatherReport/src/model/services/weatherdb.cpp
+++
@@ -1,3 +0,0 @@
-#include "weatherdb.h"
-
-WeatherDB::WeatherDB() {}
